5b4b2555713fbaea2e1fad1173eee6e8df1b7212
Artur Voroshilov <ipvoroshilov@ya.ru>
2022-09-12 18:21:32 +0300

    Completed.Task_1

---
+++ task_1/Program.cs
@@ -0,0 +1,47 @@
+﻿// ##Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+int[,] FillPrintArray(int row, int col)
+{
+    int[,] array = new int[row,col];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            array[i,j] = new Random().Next(-999,1000);
+            Console.Write($"{array[i,j]} \t");
+        }
+        Console.WriteLine();
+    }
+    return array;
+}
+try
+{
+    Console.Write("Введите количество строк: \t");
+    int m = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите количество столбцов: \t");
+    int n = Convert.ToInt32(Console.ReadLine());
+    int[,] result = FillPrintArray(m, n);
+}
+catch 
+{
+        Console.WriteLine("error");
+}
+
+// Console.Write("Введите количество строк: \t");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество столбцов: \t");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int[,] array = new int[m, n];
+// for (int i = 0; i < m; i++)
+// {
+//     for (int j = 0; j < n; j++)
+//     {
+//         array[i,j] = new Random().Next(-999,1000);
+//         Console.Write($"{array[i,j]} \t");
+//     }
+//     Console.WriteLine();
+
+// }
